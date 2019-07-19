d996a8f448a3da4bf2a28c63e5ea68d0c2300243
Matt Hoskins <mhoskins@viatechsystems.com>
2019-07-19 15:00:55 -0400

    first

---
+++ Blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    submitted_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    submitted_at = models.DateTimeField(auto_now=True)
